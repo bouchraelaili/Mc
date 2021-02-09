1e1851f6c64c4c0ef429272b196eb61b99384cf6
unknown <bouchraelaili247@gmail.com>
2021-02-09 22:24:23 +0100

    first

---
+++ frontend/js/sousCategory.js
@@ -0,0 +1,36 @@
+var urlString = window.location.search;
+var urlParam = new URLSearchParams(urlString);
+const id = urlParam.get('id');
+let sctgItemContinner = document.getElementById('sctgItemContinner');
+
+console.log(id)
+
+
+axios.get(`http://localhost:8080/sousCategory/${id}`)
+.then(function (response) {
+
+
+    
+    response.data.forEach(element => {
+        sctgItemContinner.innerHTML += `    <div class="col-lg-3">
+        <div class="D" style="margin-left: 28%;">
+          <div class="circle mx-auto">
+        
+  
+            <a href="product.html?id=${element._id}" style="font-size: 20px;
+            margin-left: 20%;
+            color: darkgreen;
+            font-weight: bold;">${element.nom}</a>
+          </div>
+        </div>
+    
+      </div>`
+
+    
+    });
+
+   
+    
+}).catch(function (err) {
+    console.log(err);
+});
